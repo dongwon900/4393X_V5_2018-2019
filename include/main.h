e1ca82f9dc4398b5461fd8c6dafb636b3257d62d
justincdavis <davis.justin@mssm.org>
2018-12-22 00:22:50 -0500

    Began to seperate into individual files

--- include/main.h
+++ include/main.h
@@ -52,7 +52,7 @@
  */
 // using namespace pros;
 // using namespace pros::literals;
-// using namespace okapi;
+using namespace okapi;
 
 /**
  * Prototypes for the competition control tasks are redefined here to ensure
@@ -67,7 +67,6 @@ void initialize(void);
 void disabled(void);
 void competition_initialize(void);
 void opcontrol(void);
-void goToWall(void);
 
 // Motor Definitions
 #define DRIVETRAIN_R_F 1
@@ -88,6 +87,38 @@ void goToWall(void);
 #define GYRO_PORT 6
 #define LIMIT_PORT 8
 
+//Motor definitions
+Motor liftMotor(LIFT_MOTOR);
+Motor forkMotor(FORK_MOTOR);
+Motor intakeMotor(INTAKE_MOTOR);
+Motor launcherMotor(LAUNCH_MOTOR);
+Motor driveLeftF(DRIVETRAIN_L_F);
+Motor driveLeftB(DRIVETRAIN_L_B);
+Motor driveRightF(DRIVETRAIN_R_F);
+Motor driveRightB(DRIVETRAIN_R_B);
+
+//Controller
+Controller controller;
+
+// Controller Buttons
+ControllerButton btnUp(ControllerDigital::R1);
+ControllerButton btnDown(ControllerDigital::R2);
+ControllerButton forkUp(ControllerDigital::L1);
+ControllerButton forkDown(ControllerDigital::L2);
+ControllerButton shootButton(ControllerDigital::A);
+ControllerButton autoDistanceButton(ControllerDigital::down);
+ControllerButton autoButton(ControllerDigital::right);
+ControllerButton driveReverseButton(ControllerDigital::X);
+ControllerButton toggleMaxSpeedButton(ControllerDigital::up);
+ControllerButton toggleIntakeButton(ControllerDigital::B);
+
+// Legacy Sensor Initialization
+ADIButton launcherLimitSwitch(LIMIT_PORT);
+pros::ADIGyro gyro(GYRO_PORT);
+pros::ADIPotentiometer liftPotentiometer (LIFT_POTENTIOMETER_PORT);
+pros::ADIUltrasonic ultrasonicRight (ULTRA_ECHO_PORT, ULTRA_PING_PORT);
+pros::ADIUltrasonic ultrasonicLeft (ULTRA_ECHO_PORT_LEFT, ULTRA_PING_PORT_LEFT);
+
 #ifdef __cplusplus
 }
 #endif
@@ -96,7 +127,7 @@ void goToWall(void);
 /**
  * You can add C++-only headers here
  */
-//#include <iostream>
+#include <iostream>
 #include <vector>
 #endif
 
