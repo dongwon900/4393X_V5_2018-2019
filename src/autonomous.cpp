315cdc134681d2e9a4e8db59d4ea27de5d699f38
justincdavis <davis.justin@mssm.org>
2019-02-05 15:40:29 -0500

    smartC

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -14,6 +14,10 @@
  * from where it left off.
  */
 
+//  size_t fread(void *ptr, size_t size_of_elements, size_t number_of_elements, FILE *a_file);
+// 
+// size_t fwrite(const void *ptr, size_t size_of_elements, size_t number_of_elements, FILE *a_file);
+
 void autonomous() {
  //  Robot robotAuto;
  //  robotAuto.launcher.enableLauncher();
