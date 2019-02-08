1f3d5a4a33a48eb360031184986a50c8c4ce20ba
Justin Davis <davis.justin@mssm.org>
2019-02-07 19:57:21 -0500

    smart

--- include/smartController.h
+++ include/smartController.h
@@ -55,6 +55,8 @@ public:
   void autoLogParser(std::vector<std::vector<int>>& autoData);
   controllerButtonState intToButtonState(int x);
   void autonomousUpdate(std::vector<std::vector<int>>& autoData);
+
+  static SmartController& instance();
 };
 
 #endif
